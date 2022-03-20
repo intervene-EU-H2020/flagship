2f5394c095f6e285a6511e22d03ee933687a617a
bnwolford <bwolford@hunt-phecodes-home.novalocal>
2022-03-20 19:51:27 +0100

    Reorg hunt specific files

--- hunt_specific/HUNT_GeneratePRS.sh
+++ hunt_specific/HUNT_GeneratePRS.sh
@@ -5,9 +5,9 @@
 
 pheno=(Alcohol_Use_Disorder Alzheimers_Disease Asthma Atrial_Fibrillation BMI Breast_Cancer CHD Chronic_Kidney_Disease Educational_Attainment Epilepsy Focal_Epilepsy Generalised_Epilepsy Gout Heart_Failure Hip_Osteoarthritis IPF ILD Inflammatory_Bowel_Disease Knee_Osteoarthritis Lifespan Lung_Cancer MDD Melanoma Osteoporosis Pain POAG Prostate_Cancer Rheumatoid_Arthritis Sleep_Apnoea smoking Stroke Subarachnoid_Haemmorhage TAA T1D T2D Thyroid_Stimulating_Hormone)
 
-output=/path/to/output
-score_directory=/path/to/score/file
-frequency_directory=/path/to/allele/frequency
+output=/mnt/scratch/brooke/PRS
+score_directory=/mnt/scratch/brooke/PRS
+frequency_directory=/mnt/scratch/brooke/PRS/bcf
 snplist_directory=/path/to/snplist
 genotype_directory=/path/to/genotype
 
@@ -18,8 +18,8 @@ pheno_i=${pheno[i]}
 
 /home/bwolford/miniconda3/bin/plink2 \
 --bfile ${genotype_directory}/genotype_plink_files \
---extract ${snplist_directory}/snplist_hg[19/38] \
+--extract ${snplist_directory}/snplist_hg19 \
 --read-freq ${frequency_directory}/frequency_file \
---score ${score_directory}/${pheno_i}_megaPRS_scores_hg[19/38].txt.gz 1 2 5 header \
+--score ${score_directory}/${pheno_i}_megaPRS_scores_hg19_varid.txt.gz 1 2 5 header \
 --out ${output}/${pheno_i}_PRS
 done
