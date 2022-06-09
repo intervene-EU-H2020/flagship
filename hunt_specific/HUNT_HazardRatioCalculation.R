dadb001f1e0af690c5fc9d17ca3edd06fd0e2e89
bnwolford <bwolford@umich.edu>
2022-06-09 09:44:47 +0000

    Need some work with the number of groups on the age stratified analysis

--- hunt_specific/HUNT_HazardRatioCalculation.R
+++ hunt_specific/HUNT_HazardRatioCalculation.R
@@ -10,11 +10,13 @@ library(broom)
 library(RNOmni)
 library(tidyr)
 
+options(warn=2)
+
+##################################################
+
 # Set variables 
-phenocols <- c("J10_ASTHMA", "C3_CANCER", "K11_APPENDACUT", "I9_AF", "C3_BREAST", "I9_CHD","C3_COLORECTAL",  "G6_EPLEPSY", "GOUT", "COX_ARTHROSIS", "KNEE_ARTHROSIS", "F5_DEPRESSIO", "C3_MELANOMA_SKIN", "C3_PROSTATE",  "I9_SAH", "T1D", "T2D", "ILD", "C3_BRONCHUS_LUNG")
-prscols <- c("Asthma","AllCancers","Appendicitis", "Atrial_Fibrillation", "Breast_Cancer", "CHD","Colorectal_Cancer", "Epilepsy","Gout", "Hip_Osteoarthritis", "Knee_Osteoarthritis","MDD", "Melanoma", "Prostate_Cancer", "Subarachnoid_Haemmorhage", "T1D","T2D", "ILD", "Lung_Cancer")
-#,  "Rheumatoid_Arthritis"
-#""RHEUMA_SEROPOS_OTHER",
+phenocols <- c("J10_ASTHMA", "C3_CANCER", "K11_APPENDACUT", "I9_AF", "C3_BREAST", "I9_CHD","C3_COLORECTAL",  "G6_EPLEPSY", "GOUT", "COX_ARTHROSIS", "KNEE_ARTHROSIS", "F5_DEPRESSIO", "C3_MELANOMA_SKIN", "C3_PROSTATE", "RHEUMA_SEROPOS_OTH", "I9_SAH", "T1D", "T2D", "ILD", "C3_BRONCHUS_LUNG")
+prscols <- c("Asthma","AllCancers","Appendicitis", "Atrial_Fibrillation", "Breast_Cancer", "CHD","Colorectal_Cancer", "Epilepsy","Gout", "Hip_Osteoarthritis", "Knee_Osteoarthritis","MDD", "Melanoma", "Prostate_Cancer","Rheumatoid_Arthritis", "Subarachnoid_Haemmorhage", "T1D","T2D", "ILD", "Lung_Cancer")
 
 pheno_file="/home/bwolford/workbench/intervene/endpointsPhenoFormatHUNT.csv" #path to phenotype file 
 prs_path="/home/bwolford/scratch/brooke/scores/" #path to PRS files
@@ -140,12 +142,6 @@ write.table(surv_sd_results,paste0(output_dir,"/survival_perSD_all.csv"),sep=","
 ###########################################################################################################################################################################################################################################################
 
 #Sex specific HRs
-
-phenocols <- c("J10_ASTHMA", "C3_CANCER", "K11_APPENDACUT", "I9_AF", "C3_BREAST", "I9_CHD","C3_COLORECTAL",  "G6_EPLEPSY", "GOUT", "COX_ARTHROSIS", "KNEE_ARTHROSIS", "F5_DEPRESSIO", "C3_MELANOMA_SKIN", "C3_PROSTATE",  "I9_SAH", "T1D", "T2D", "ILD", "C3_BRONCHUS_LUNG")
-prscols <- c("Asthma","AllCancers","Appendicitis", "Atrial_Fibrillation", "Breast_Cancer", "CHD","Colorectal_Cancer", "Epilepsy","Gout", "Hip_Osteoarthritis", "Knee_Osteoarthritis","MDD", "Melanoma", "Prostate_Cancer", "Subarachnoid_Haemmorhage", "T1D","T2D", "ILD", "Lung_Cancer")
-#,  "Rheumatoid_Arthritis"
-#""RHEUMA_SEROPOS_OTHER",
-
 maleresults <- c()
 femaleresults <- c()
 
@@ -256,12 +252,7 @@ write.csv(femaleresults, paste0(output_dir,"HUNT_FemaleSample.csv"))
 
 ###########################################################################################################################################################################################################################################################
 ###########################################################################################################################################################################################################################################################
-
-phenocols <- c("J10_ASTHMA", "C3_CANCER", "K11_APPENDACUT", "I9_AF", "C3_BREAST", "I9_CHD","C3_COLORECTAL",  "G6_EPLEPSY", "GOUT", "COX_ARTHROSIS", "KNEE_ARTHROSIS", "F5_DEPRESSIO", "C3_MELANOMA_SKIN", "C3_PROSTATE",  "I9_SAH", "T1D", "T2D", "ILD", "C3_BRONCHUS_LUNG")
-prscols <- c("Asthma","AllCancers","Appendicitis", "Atrial_Fibrillation", "Breast_Cancer", "CHD","Colorectal_Cancer", "Epilepsy","Gout", "Hip_Osteoarthritis", "Knee_Osteoarthritis","MDD", "Melanoma", "Prostate_Cancer", "Subarachnoid_Haemmorhage", "T1D","T2D", "ILD", "Lung_Cancer")
-#,  "Rheumatoid_Arthritis"
-#""RHEUMA_SEROPOS_OTHER",
-
+######## Sex*PGS interaction ##########
 results <- c()
 
 for(i in 1:length(phenocols)){
@@ -280,8 +271,8 @@ for(i in 1:length(phenocols)){
   #Subset columns to the IDs and score only. Note: columns 1 or 2 may be redundant and can be removed if necessary. Kept in to avoid bugs.
   PRS <- PRS[,c(1,2,5)]
   
-  #Rename ID column to the name of the ID column in the 
-  colnames(PRS) <- c("ENTER_ID", "ENTER_ID", paste0(prscols[i],"_prs"))
+  #Rename ID column to the name of the ID column in the phenotype file
+  colnames(PRS) <- c("FID", pheno_file_ID, paste0(prscols[i],"_prs"))
   
   #left_join to the phenotype file
   pheno <- left_join(pheno, PRS)
@@ -308,12 +299,14 @@ for(i in 1:length(phenocols)){
   #Perform survival analysis
   survival <- coxph(as.formula(paste0("Surv(AGE,",phenocols[i],") ~ ",prscols[i],"_prs + SEX + ", prscols[i], "_prs:SEX + PC1 + PC2 + PC3 + PC4 + PC5 + PC6 + PC7 + PC8 + PC9 + PC10 + BATCH")), data=pheno, na.action=na.exclude)
   
+  #Note: changed _prs:SEXmale to just SEX because we have 1/2 as opposed to male/female, reference should stay the same
+  
   #Extract hazard ratios, betas, standard errors and p-vals
   phenotype <- rep(phenocols[i],10)
   prs <- rep(prscols[i],10)
-  betas <- summary(survival)$coefficients[paste0(prscols[i],"_prs:SEXmale"),"coef"]
-  std_errs <- summary(survival)$coefficients[paste0(prscols[i],"_prs:SEXmale"),"se(coef)"]
-  pvals <- summary(survival)$coefficients[paste0(prscols[i],"_prs:SEXmale"),"Pr(>|z|)"]
+  betas <- summary(survival)$coefficients[paste0(prscols[i],"_prs:SEX"),"coef"]
+  std_errs <- summary(survival)$coefficients[paste0(prscols[i],"_prs:SEX"),"se(coef)"]
+  pvals <- summary(survival)$coefficients[paste0(prscols[i],"_prs:SEX"),"Pr(>|z|)"]
   OR <- exp(betas)
   CIpos <- exp(betas+1.96*std_errs)
   CIneg <- exp(betas-1.96*std_errs)
@@ -326,16 +319,30 @@ write.csv(results, paste0(output_dir,"HUNT_SexInteraction.csv"))
 
 ###########################################################################################################################################################################################################################################################
 ###########################################################################################################################################################################################################################################################
+################ Age stratified ###########
 
-phenocols <- c("J10_ASTHMA", "C3_CANCER", "K11_APPENDACUT", "I9_AF", "C3_BREAST", "I9_CHD","C3_COLORECTAL",  "G6_EPLEPSY", "GOUT", "COX_ARTHROSIS", "KNEE_ARTHROSIS", "F5_DEPRESSIO", "C3_MELANOMA_SKIN", "C3_PROSTATE",  "I9_SAH", "T1D", "T2D", "ILD", "C3_BRONCHUS_LUNG")
-prscols <- c("Asthma","AllCancers","Appendicitis", "Atrial_Fibrillation", "Breast_Cancer", "CHD","Colorectal_Cancer", "Epilepsy","Gout", "Hip_Osteoarthritis", "Knee_Osteoarthritis","MDD", "Melanoma", "Prostate_Cancer", "Subarachnoid_Haemmorhage", "T1D","T2D", "ILD", "Lung_Cancer")
-#,  "Rheumatoid_Arthritis"
-#""RHEUMA_SEROPOS_OTHER",
+#these are the phenotypes that have <1 case in the bottom PRS percentile 
+#table(pheno_split$Atrial_Fibrillation_group,pheno_split$tgroup,pheno_split$event)
+phenocols_10 <- c("I9_AF", "C3_BREAST","C3_COLORECTAL",  "GOUT", "C3_MELANOMA_SKIN", "RHEUMA_SEROPOS_OTH","C3_PROSTATE", "T2D","ILD","C3_BRONCHUS_LUNG")
+phenocols_9<-c("G6_EPLEPSY")
 
+phenocols <- c("J10_ASTHMA", "C3_CANCER",  "I9_AF", "C3_BREAST", "I9_CHD","C3_COLORECTAL",  "G6_EPLEPSY", "GOUT", "COX_ARTHROSIS", "KNEE_ARTHROSIS", "F5_DEPRESSIO", "C3_MELANOMA_SKIN", "C3_PROSTATE", "RHEUMA_SEROPOS_OTH", "T2D", "ILD", "C3_BRONCHUS_LUNG")
+prscols <- c("Asthma","AllCancers", "Atrial_Fibrillation", "Breast_Cancer", "CHD","Colorectal_Cancer", "Epilepsy","Gout", "Hip_Osteoarthritis", "Knee_Osteoarthritis","MDD", "Melanoma", "Prostate_Cancer","Rheumatoid_Arthritis", "T2D", "ILD", "Lung_Cancer")
+##"K11_APPENDACUT",
+#"Appendicitis",
+#c(21.27,32.49,46.77), #K11_APPENDACUT
+
+#"I9_SAH",
+#"Subarachnoid_Haemmorhage",
+#c(43.46,54.54,65.66), #I9_SAH
+
+#T1D
+#T1D
+#c(12.62,19.73,33.28), #T1D
 #Ages are based on mean quartiles from biobanks to be used in the lifetime risk estimation
-ages <- data.frame(c(53.34,63.54,72.24), #C3_CANCER
-                   c(21.27,32.49,46.77), #K11_APPENDACUT
+ages <- data.frame(
                    c(31.37,48.53,62.07), #J10_ASTHMA
+                   c(53.34,63.54,72.24), #C3_CANCER
                    c(61.51,70.32,77.77), #I9_AF
                    c(50.68,58.83,67.26), #C3_BREAST
                    c(55.86,64.18,72.35), #I9_CHD
@@ -347,9 +354,7 @@ ages <- data.frame(c(53.34,63.54,72.24), #C3_CANCER
                    c(31.98,44.01,57.19), #F5_DEPRESSIO
                    c(46.51,59.02,69.38), #C3_MELANOMA_SKIN
                    c(62.60,68.25,73.89), #C3_PROSTATE
-                   #RHEUMA_SEROPOS_OTH TO BE COMPLETED
-                   c(43.46,54.54,65.66), #I9_SAH
-                   c(12.62,19.73,33.28), #T1D
+                   c(50,65,77),#RHEUMA_SEROPOS_OTH TO BE COMPLETED BASED ON REAL DATA. 
                    c(54.36,63.04,71.13), #T2D
                    c(51.22,61.59,71.48), #ILD
                    c(60.79,68.02,75.52)) #C3_BRONCHUS_LUNG
@@ -358,6 +363,7 @@ results <- c()
 
 for(i in 1:length(phenocols)){
   age <- c(ages[1,i],ages[2,i],ages[3,i])
+  print(phenocols[i])
   
   #Read in phenotype file
   pheno <- fread(input=pheno_file, select=c("ID","DATE_OF_BIRTH","SEX","PC1","PC2","PC3","PC4","PC5","PC6","PC7","PC8","PC9","PC10","ANCESTRY",phenocols[i],paste0(phenocols[i],"_DATE"),"END_OF_FOLLOWUP","BATCH"), data.table=FALSE)
@@ -383,16 +389,48 @@ for(i in 1:length(phenocols)){
   #Feel free to subset using your own code: only provided as a reminder.
   pheno <- subset(pheno, ANCESTRY=='EUR')
   
-  #Assign PRS into percentiles
-  q <- quantile(pheno[[paste0(prscols[i],"_prs")]], probs=c(0,0.01,0.05,0.1,0.2,0.4,0.6,0.8,0.9,0.95,0.99,1))
+  if (!phenocols[i] %in% phenocols_10 & !phenocols[i] %in% phenocols_9) { #if the phenotype is not in the 10 group list, proceed as normal
+    #Assign PRS into percentiles
+    q <- quantile(pheno[[paste0(prscols[i],"_prs")]], probs=c(0,0.01,0.05,0.1,0.2,0.4,0.6,0.8,0.9,0.95,0.99,1))
   
-  pheno[[paste0(prscols[i],"_group")]] <- cut(pheno[[paste0(prscols[i],"_prs")]], q, include.lowest=TRUE,
+    pheno[[paste0(prscols[i],"_group")]] <- cut(pheno[[paste0(prscols[i],"_prs")]], q, include.lowest=TRUE,
                                               labels=paste("Group",1:11))
-  
-  #Make all necessary variables factors
-  pheno$BATCH <- as.factor(pheno$BATCH)
-  pheno[[paste0(prscols[i],"_group")]] <- as.factor(pheno[[paste0(prscols[i],"_group")]])
-  pheno[[paste0(prscols[i],"_group")]] <- relevel(pheno[[paste0(prscols[i],"_group")]], ref="Group 6")
+    #Make all necessary variables factors
+    pheno$BATCH <- as.factor(pheno$BATCH)
+    pheno[[paste0(prscols[i],"_group")]] <- as.factor(pheno[[paste0(prscols[i],"_group")]])
+    pheno[[paste0(prscols[i],"_group")]] <- relevel(pheno[[paste0(prscols[i],"_group")]], ref="Group 6")
+    
+    group_nums<-c(1:5,7:11) #numbers for results printing 
+    matrix_row<-10
+    }else if (phenocols[i] %in% phenocols_9) { #need to collapse top and bottom groupings 
+      print("use 9 groups")
+      #Assign PRS into percentiles
+      q <- quantile(pheno[[paste0(prscols[i],"_prs")]], probs=c(0,0.05,0.1,0.2,0.4,0.6,0.8,0.9,0.95,1))
+      
+      pheno[[paste0(prscols[i],"_group")]] <- cut(pheno[[paste0(prscols[i],"_prs")]], q, include.lowest=TRUE,
+                                                  labels=paste("Group",1:9))
+      #Make all necessary variables factors
+      pheno$BATCH <- as.factor(pheno$BATCH)
+      pheno[[paste0(prscols[i],"_group")]] <- as.factor(pheno[[paste0(prscols[i],"_group")]])
+      pheno[[paste0(prscols[i],"_group")]] <- relevel(pheno[[paste0(prscols[i],"_group")]], ref="Group 5")
+      
+      group_nums<-c(1:4,6:9) #numbers for results printing 
+      matrix_row<-8
+  } else { #use 10 groups instead of 11
+    #Assign PRS into percentiles
+    print("use 10 groups")
+    q <- quantile(pheno[[paste0(prscols[i],"_prs")]], probs=c(0,0.05,0.1,0.2,0.4,0.6,0.8,0.9,0.95,0.99,1))
+    
+    pheno[[paste0(prscols[i],"_group")]] <- cut(pheno[[paste0(prscols[i],"_prs")]], q, include.lowest=TRUE,
+                                                labels=paste("Group",1:10))
+    #Make all necessary variables factors
+    pheno$BATCH <- as.factor(pheno$BATCH)
+    pheno[[paste0(prscols[i],"_group")]] <- as.factor(pheno[[paste0(prscols[i],"_group")]])
+    pheno[[paste0(prscols[i],"_group")]] <- relevel(pheno[[paste0(prscols[i],"_group")]], ref="Group 5")
+    
+    group_nums<-c(1:4,6:10) #numbers for results printing 
+    matrix_row<-9
+  }
   
   #Specify age as either the Age at Onset or End of Follow-up (if not a case)
   pheno$AGE <- ifelse(pheno[[phenocols[i]]]==1, time_length(difftime(pheno[[paste0(phenocols[i],"_DATE")]], pheno$DATE_OF_BIRTH), 'years'), time_length(difftime(pheno$END_OF_FOLLOWUP, pheno$DATE_OF_BIRTH), 'years'))
@@ -420,26 +458,40 @@ for(i in 1:length(phenocols)){
     #Perform survival analysis
     survival <- coxph(as.formula(paste0("Surv(tstart, AGE, event) ~ ",prscols[i],"_group + PC1 + PC2 + PC3 + PC4 + PC5 + PC6 + PC7 + PC8 + PC9 + PC10 + BATCH")), data=pheno_split_sub, na.action=na.exclude)
     
-    controls <- table(pheno_split_sub[[paste0(prscols[i],"_group")]], pheno_split_sub[["event"]])[2:11,1]
-    cases <- if(sum(nrow(pheno_split_sub[pheno_split_sub[["event"]]==0,])) == length(pheno_split_sub[["event"]])){ 
-      rep(0,10)} else {table(pheno_split_sub[[paste0(prscols[i],"_group")]], pheno_split_sub[["event"]])[2:11,2]}
+    #handle differing group numbers
+    if (!phenocols[i] %in% phenocols_10 & !phenocols[i] %in% phenocols_9) { #if the phenotype is not in the 10 gorup list, proceed as normal
+      controls <- table(pheno_split_sub[[paste0(prscols[i],"_group")]], pheno_split_sub[["event"]])[2:11,1]
+      cases <- if(sum(nrow(pheno_split_sub[pheno_split_sub[["event"]]==0,])) == length(pheno_split_sub[["event"]])){ 
+        rep(0,10)} else {table(pheno_split_sub[[paste0(prscols[i],"_group")]], pheno_split_sub[["event"]])[2:11,2]}
+    } else if (phenocols[i] %in% phenocols_9) {
+        controls <- table(pheno_split_sub[[paste0(prscols[i],"_group")]], pheno_split_sub[["event"]])[2:9,1]
+        cases <- if(sum(nrow(pheno_split_sub[pheno_split_sub[["event"]]==0,])) == length(pheno_split_sub[["event"]])){ 
+          rep(0,8)} else {table(pheno_split_sub[[paste0(prscols[i],"_group")]], pheno_split_sub[["event"]])[2:9,2]}
+    } else {
+      controls <- table(pheno_split_sub[[paste0(prscols[i],"_group")]], pheno_split_sub[["event"]])[2:10,1]
+      cases <- if(sum(nrow(pheno_split_sub[pheno_split_sub[["event"]]==0,])) == length(pheno_split_sub[["event"]])){ 
+        rep(0,9)} else {table(pheno_split_sub[[paste0(prscols[i],"_group")]], pheno_split_sub[["event"]])[2:10,2]}
+    }
     
     #Extract hazard ratios, betas, standard errors and p-vals
-    phenotype <- rep(phenocols[i],10)
-    prs <- rep(prscols[i],10)
-    minage <- rep(min(pheno_split_sub$tstart), 10)
-    maxage <- rep(max(pheno_split_sub$AGE), 10)
-    medianAAO <- rep(median(pheno_split_sub[pheno_split_sub$event==1,"AGE"], na.rm=TRUE),10)
-    group <- c(paste0(prscols[i],"_groupGroup ",c(1:5,7:11)))
+    phenotype <- rep(phenocols[i],matrix_row)
+    prs <- rep(prscols[i],matrix_row)
+    minage <- rep(min(pheno_split_sub$tstart), matrix_row)
+    maxage <- rep(max(pheno_split_sub$AGE), matrix_row)
+    medianAAO <- rep(median(pheno_split_sub[pheno_split_sub$event==1,"AGE"], na.rm=TRUE),matrix_row)
+    group <- c(paste0(prscols[i],"_groupGroup ",group_nums))
     betas <- summary(survival)$coefficients[group,"coef"]
     std_errs <- summary(survival)$coefficients[group,"se(coef)"]
     pvals <- summary(survival)$coefficients[group,"Pr(>|z|)"]
     OR <- exp(betas)
     CIpos <- exp(betas+1.96*std_errs)
     CIneg <- exp(betas-1.96*std_errs)
-    result <- matrix(c(phenotype, prs, minage, maxage, medianAAO, controls, cases, group, betas, std_errs, pvals, OR, CIpos, CIneg), nrow=10, ncol=14)
+    result <- matrix(c(phenotype,prs,minage, maxage, medianAAO, controls, cases, group, betas, std_errs, pvals, OR, CIpos, CIneg), nrow=matrix_row, ncol=14)
     results <- rbind(results, result)
   }
 }
 
-write.csv(results, paste0(output_dir,"HUNT_AgeStratifiedResults.csv"))
+results<-data.frame(results)
+names(results)<-c("pheno","prs","minage", "maxage", "medianAAO", "controls", "cases", "group", "betas", "std_errs", "pvals", "HR", "CIpos", "CIneg")
+write.csv(results, paste0(output_dir,"HUNT_AgeStratifiedResults.csv"),row.names=FALSE,quote=FALSE)
+
