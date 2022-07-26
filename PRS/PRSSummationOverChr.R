09ea079a5e476b91efe6fdf99a8dafb8223c20dd
Bradley Jermy <39912643+Bjermy@users.noreply.github.com>
2022-07-26 08:58:42 +0300

    Update PRSSummationOverChr.R

--- PRS/PRSSummationOverChr.R
+++ PRS/PRSSummationOverChr.R
@@ -18,17 +18,15 @@ for(i in 1:length(pheno)){
     score <- fread(input=paste0("/path/to/PRSscore/files/",pheno[i],"_PRS_chr",j,".sscore"), data.table=FALSE)
   
     #Take IID, ALLELE_CT and SCORE1_AVG
-    score <- score[,c("IID","ALLELE_CT","SCORE1_AVG")]
-    score[[paste0("PRS_Sum_",j)]] <- score$ALLELE_CT * score$SCORE1_AVG
-  	colnames(score)[2] <- paste0("ALLELE_CT_",j)
+    score <- score[,c("IID","SCORE1_SUM")]
+  	colnames(score)[2] <- paste0(pheno[i],"_SUM_",j)
   	
     #Drop average score as when next chromosome added in cannot join. Left join with the sum and the IID
-    score <- score[,-3]
     FinalScore <- left_join(FinalScore, score)
     print(dim(FinalScore))
   }
 
-  FinalScore[[paste0(pheno[i],"_PRS")]] <- rowSums(FinalScore[,c(seq(3,45,by=2))]) / rowSums(FinalScore[,c(seq(2,44,by=2))])
+  FinalScore[["SCORE1_SUM"]] <- rowSums(FinalScore[,c(2:23)]) 
   print(head(FinalScore))  
   fwrite(FinalScore, paste0("/path/to/PRSscore/files/",pheno[i],"_PRS"), sep="\t")
 }
