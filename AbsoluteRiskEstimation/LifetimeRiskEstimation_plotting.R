506fc3a821e31bcc0aae1405aa9d4c7a4fd0d471
bnwolford <bwolford@hunt-phecodes-home.novalocal>
2022-10-21 13:52:20 +0200

    Moving differnece between risk strata risk plotting to this script

--- AbsoluteRiskEstimation/LifetimeRiskEstimation_plotting.R
+++ AbsoluteRiskEstimation/LifetimeRiskEstimation_plotting.R
@@ -30,7 +30,7 @@ pdf(file=paste0(output_dir,"Risks_Facet.pdf"),height=10,width=10,useDingbats=TRU
 colors<-c("#824A85","#2C6687","#3C884B")
 ggplot(riskwithintervals, aes(Age, LifetimeRisk, fill=Group, color=Group, group=Group)) +
   stat_smooth(method = "lm", formula = y ~ poly(x, 13), se = FALSE) +
-  geom_point() +
+  geom_point(alpha=0.5) +
   geom_ribbon(aes(ymin=CIneg, ymax=CIpos, fill=Group), alpha=0.2) +
   xlab("Age Range") + 
   ylab("Absolute Cumulative Risk (%)") + 
@@ -47,4 +47,90 @@ ggplot(riskwithintervals, aes(Age, LifetimeRisk, fill=Group, color=Group, group=
         axis.title.y = element_text(size = 18),
         axis.text.y = element_text(size = 16))
  dev.off()
+ 
+ 
+############################# comparisons across strata 
+ 
+ hr_phenos <- c("C3_PROSTATE","C3_BREAST", "K11_APPENDACUT", "J10_ASTHMA", "I9_AF", "C3_BREAST", "I9_CHD", "C3_COLORECTAL", "G6_EPLEPSY", "GOUT", "COX_ARTHROSIS", "KNEE_ARTHROSIS", "F5_DEPRESSIO", "C3_MELANOMA_SKIN", "C3_PROSTATE", "RHEUMA_SEROPOS_OTH", "T1D", "T2D")
+ gbd_phenos <- c("Prostate cancer", "Breast cancer", "Appendicitis", "Asthma", "Atrial fibrillation and flutter", "Breast cancer", "Ischemic heart disease", "Colon and rectum cancer", "Idiopathic epilepsy", "Gout", "Osteoarthritis hip", "Osteoarthritis knee", "Major depressive disorder", "Malignant skin melanoma", "Prostate cancer", "Rheumatoid arthritis", "Diabetes mellitus type 1", "Diabetes mellitus type 2")
+ 
+ #for now remove total cancer because estbb has weird group1 and group 2 lifetime risk flip
+ biobanks<-c("EstBB","HUNT","UKB","FinnGen","BBJ","MGB","MGB")
+ 
+ ancestries<-c("EUR","SAS","EAS","AFR")
+ 
+ rel_differences<-data.frame(NULL)
+ differences<-data.frame(NULL)
+ for(j in 1:length(hr_phenos)){
+   for (b in 1:length(biobanks)){
+     for (a in 1:length(ancestries)){
+       #file<-paste0("/mnt/work/workbench/bwolford/intervene/results/",hr_phenos[j],"_LifetimeRisk_BootstrappedConfidenceIntervals_",biobanks[b],".csv")
+       file<-paste0("/home/bwolford/scratch/brooke/results/",hr_phenos[j],"_LifetimeRisk_BootstrappedConfidenceIntervals_",biobanks[b],"_",ancestries[a],".csv")
+       if (file.exists(file)){
+         df<-fread(file)
+         
+         #Average PRS cumulative risk at age 80
+         avg<-df %>% filter(Group %in% c("Group6") & str_detect(Age,"75 to 79")) %>% select(CIneg,CIpos,LifetimeRisk)
+         avg$biobank<-biobanks[b]
+         avg$pheno<-gbd_phenos[j]
+         avg$comparison<-"40-60%"
+         avg$ancestry<-ancestries[a]
+         
+         top<-df %>% filter(Group %in% c("Group11") & str_detect(Age,"75 to 79")) %>% select(CIneg,CIpos,LifetimeRisk)
+         top$biobank<-biobanks[b]
+         top$pheno<-gbd_phenos[j]
+         top$comparison<-">99%"
+         top$ancestry<-ancestries[a]
+         
+         bottom<-df %>% filter(Group %in% c("Group1") & str_detect(Age,"75 to 79")) %>% select(CIneg,CIpos,LifetimeRisk)
+         bottom$biobank<-biobanks[b]
+         bottom$pheno<-gbd_phenos[j]
+         bottom$comparison<-"<1%"
+         bottom$ancestry<-ancestries[a]
+         
+         if (nrow(bottom)==1 & nrow(top)==1){
+           diff<-data.frame(biobank=biobanks[b], pheno=gbd_phenos[j],comparison="top vs bottom", ancestry=ancestries[a], 
+                            CIneg=abs(top$CIneg-bottom$CIneg), CIpos=abs(top$CIpos-bottom$CIpos), LifetimeRisk=abs(top$LifetimeRisk-bottom$LifetimeRisk))
+         }
+         ####need to handle when Group10 is the top percentile, maybe take max of the Group 10 and Group 11 if both exist?
+         
+         rel_differences<-rbind(rel_differences,avg,top,bottom)
+         differences<-rbind(differences,diff)
+       }}}}
+ 
+ rel_differences$comparison<-factor(rel_differences$comparison,levels=c("<1%","40-60%",">99%"))
+ rel_differences<-rel_differences %>% filter(str_detect(ancestry,"EUR")|str_detect(ancestry,"EAS"))
+ png(file="/mnt/work/workbench/bwolford/intervene/2022_10_06/RiskEstimates/cumulative_risk_differences.png",units="in",res=300,height=6,width=10)
+ ggplot(rel_differences,aes(x=LifetimeRisk,y=pheno,color=biobank)) + geom_point(size=3,alpha=0.8) +
+   theme_bw() + geom_errorbarh(aes(xmin=CIneg,xmax=CIpos),height=0.4) + facet_wrap(~comparison) +
+   labs(x="Cumulative Risk at age 80",y='Trait') +
+   theme(title = element_text(size = 22),
+         legend.text = element_text(size = 16),
+         legend.title = element_text(size = 18),
+         axis.title.x = element_text(size = 18),
+         axis.text.x = element_text(size = 12),
+         axis.title.y = element_text(size = 18),
+         axis.text.y = element_text(size = 16)) +
+   scale_color_manual(values=carto_pal(n=length(biobanks), name="Safe"))
+ dev.off()
+ 
+ differences<-differences %>% filter(str_detect(ancestry,"EUR")|str_detect(ancestry,"EAS"))
+ png(file="/mnt/work/workbench/bwolford/intervene/2022_10_06/RiskEstimates/cumulative_risk_top_vs_bottom.png",units="in",res=300,height=6,width=10)
+ ggplot(differences,aes(x=LifetimeRisk,y=pheno,color=biobank)) + geom_point(size=3,alpha=0.8) +
+   theme_bw() + geom_errorbarh(aes(xmin=CIneg,xmax=CIpos),height=0.4) + 
+   labs(x="Difference in absolute cumulative risk between >99% and <1% ",y='Trait') +
+   theme(title = element_text(size = 22),
+         legend.text = element_text(size = 16),
+         legend.title = element_text(size = 18),
+         axis.title.x = element_text(size = 18),
+         axis.text.x = element_text(size = 12),
+         axis.title.y = element_text(size = 18),
+         axis.text.y = element_text(size = 16)) +
+   geom_vline(xintercept=0,linetype="dashed",color="black",alpha=0.8) +
+   scale_color_manual(values=carto_pal(n=length(biobanks), name="Safe"))
+ dev.off()
+ 
+ 
+ 
+ 
   
