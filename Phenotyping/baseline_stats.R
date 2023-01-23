de82875171153b6907e5f3358f05e4ec6daa4702
bnwolford <bwolford@umich.edu>
2023-01-23 06:20:55 +0000

    Adding proportion of cases total to summary output

--- Phenotyping/baseline_stats.R
+++ Phenotyping/baseline_stats.R
@@ -13,6 +13,7 @@ output_dir<-"path/to/output/directory/" #keeping the final "/" is required
 #output_dir<-getwd()
 drop<-c(NA) #if you are missing a phenotype, list the name here
 birth_as_baseline<-FALSE #if you are using birth as the baseline, change to TRUE
+ancestry<-"ancestry" #add ancestry EUR, SAS, EAS, AFR which the phenotype file corresponds to
 
 #read in data 
 df<-fread(file_name)
@@ -28,6 +29,8 @@ df$SEX<-recode(df$SEX, `2`="female", `1`="male", .default = NA_character_)
 p<- c("C3_CANCER", "K11_APPENDACUT", "J10_ASTHMA", "I9_AF", "C3_BREAST", "I9_CHD", "C3_COLORECTAL", "G6_EPLEPSY", "GOUT", "COX_ARTHROSIS", "KNEE_ARTHROSIS", "F5_DEPRESSIO", 
                "C3_MELANOMA_SKIN", "C3_PROSTATE", "RHEUMA_SEROPOS_OTH", "T1D", "T2D", "ILD", "C3_BRONCHUS_LUNG")
 p<-p[!(p %in% drop)] #drop any phenotypes you don't have to avoid errors later
+
+p<- c("C3_CANCER", "K11_APPENDACUT")
 
 #phenotype file must have "SEX", DATE_OF_BIRTH", "START_OF_FOLLOWUP","END_OF_FOLLOWUP" in YYYY-MM-DD format
 #each of the phenotypes above must have a matching column which is the same name with "_DATE" appended
@@ -114,6 +117,40 @@ for (idx in 1:length(p)){
     n_female_control<-nrow(df %>% filter(SEX_NUM==0 & get(p[idx])==0))
     female_perc_cases<-n_female_case/n_case*100
     female_perc_controls<-n_female_control/n_control*100
+    
+    ##### proportion of total cases in age of diagnosis bins
+    #all 
+    tmp<-df %>% filter(get(p[idx])==1) %>% mutate(age=as.numeric(difftime(as.POSIXct(get(date_col)),DATE_OF_BIRTH,units="days"))/365.5)
+    #lifetimerisks <- data.frame(age=c("1 to 4","5 to 9","10 to 14","15 to 19","20 to 24","25 to 29","30 to 34","35 to 39","40 to 44","45 to 49","50 to 54","55 to 59","60 to 64","65 to 69","70 to 74","75 to 79"))
+    tmp$bins<-cut_width(tmp$age,width=5, center=2.5,closed="left") #cut into lifetime risk bins
+    total<-as.numeric(tmp %>% summarize(sum(get(p[idx]))))
+    counts<- tmp %>% group_by(bins) %>% count(p[idx])
+    counts$prop<-counts$n/total*100
+    counts$sample<-"all"
+    
+    #female
+    tmp<-df %>% filter(get(p[idx])==1 & SEX_NUM==0) %>% mutate(age=as.numeric(difftime(as.POSIXct(get(date_col)),DATE_OF_BIRTH,units="days"))/365.5)
+    tmp$bins<-cut_width(tmp$age,width=5, center=2.5,closed="left") #cut into lifetime risk bins
+    total<-as.numeric(tmp %>% summarize(sum(get(p[idx]))))
+    counts_f<-tmp %>% group_by(bins) %>% count(p[idx])
+    counts_f$prop<-counts_f$n/total*100
+    counts_f$sample<-"female"
+    
+    #male
+    tmp<-df %>% filter(get(p[idx])==1 & SEX_NUM==1) %>% mutate(age=as.numeric(difftime(as.POSIXct(get(date_col)),DATE_OF_BIRTH,units="days"))/365.5)
+    tmp$bins<-cut_width(tmp$age,width=5, center=2.5,closed="left") #cut into lifetime risk bins
+    total<-as.numeric(tmp %>% summarize(sum(get(p[idx]))))
+    counts_m<-tmp %>% group_by(bins) %>% count(p[idx])
+    counts_m$prop<-counts_m$n/total*100
+    counts_m$sample<-"male"
+    
+    counts<-rbind(counts,counts_f,counts_m)
+    
+    
+    #coxph(as.formula(paste0("Surv(AGE,",phenocols[i],") ~ ",prscols[i],"_group")))
+    
+    #] is inclusive, ) is exclusive
+    
   }
   if (idx==1){
     summary_stats_df<-data.frame(p[idx],n_case,n_control,prev,age_recruitment_median,age_recruitment_IQR,
@@ -121,19 +158,36 @@ for (idx in 1:length(p)){
                                  age_recruitment_median_controls,age_recruitment_IQR_controls,
                                  age_onset_median,age_onset_IQR,follow_up_median,follow_up_IQR,
                                  age_corr,sex_corr,n_female_case,n_female_control,female_perc_cases,female_perc_controls)
+    
+    counts_df<-counts
+    
   } else{
     summary_stats_df<-rbind(summary_stats_df,data.frame(p[idx],n_case,n_control,prev,age_recruitment_median,age_recruitment_IQR,
                                                               age_recruitment_median_cases,age_recruitment_IQR_cases,
                                                               age_recruitment_median_controls,age_recruitment_IQR_controls,
                                                               age_onset_median,age_onset_IQR,follow_up_median,follow_up_IQR,
                                                               age_corr,sex_corr,n_female_case,n_female_control,female_perc_cases,female_perc_controls))
-  }
+  
+    counts_df<-rbind(counts_df,counts)
+    
+    }
 }
 
-
+#write age data frame
 names(age_df)<-c("trait","X25","X50","X75")
 write.csv(format(age_df,digits=3),paste0(output_dir,biobank,"_age_quartiles.csv"),row.names=FALSE,quote=FALSE)
 
+#write counts data frame
+names(counts_df)<-c("bins","pheno","count","prop","sample")
+write.csv(format(counts_df,digits=3),paste0(output_dir,biobank,"_prevalences.csv"),row.names=FALSE,quote=FALSE)
+
+#plot 
+pdf(file=paste0(output_dir,biobank,"proportions.pdf"),height=4,width=12)
+ggplot(counts_df,aes(x=bins,y=prop,color=pheno)) + geom_point() + facet_wrap(~sample) + 
+theme(axis.text.x = element_text(angle = 45, hjust = 1))
+dev.off()
+
+#write summary stats data frame
 names(summary_stats_df)<-c("trait","cases","controls","prevalence","age_recruitment_median","age_recruitment_IQR",
                                  "age_recruitment_median_cases","age_recruitment_IQR_cases",
                                  "age_recruitment_median_controls","age_recruitment_IQR_controls",
@@ -141,3 +195,5 @@ names(summary_stats_df)<-c("trait","cases","controls","prevalence","age_recruitm
                                  "sex_corr","n_cases_female","n_controls_female",
                                  "female_prev_cases","female_prev_controls")
 write.csv(format(summary_stats_df,digits=3),paste0(output_dir,biobank,"_baseline_summary_stats.csv"),row.names=FALSE,quote=FALSE)
+
+
